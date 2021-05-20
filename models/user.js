bde38dfac8d0eace743d259cbc929b07e3e26aa4
Flipmemax <maxphoogland@gmail.com>
2021-05-20 21:17:37 +0200

    add relations user-images

--- models/user.js
+++ models/user.js
@@ -8,6 +8,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
+      user.hasMany(models.image);
       // define association here
     }
   }
--- routers/image.js
+++ routers/image.js
@@ -1,26 +1,8 @@
-const { Router } = require("express");
+const { Router, response } = require("express");
 const Image = require("../models").image;
-const { toData } = require("../auth/jwt");
+const User = require("../models").user;
 
 const router = new Router();
-
-router.get("/auth/messy", async (req, res, next) => {
-  const auth =
-    req.headers.authorization && req.headers.authorization.split(" ");
-  if (auth && auth[0] === "Bearer" && auth[1]) {
-    try {
-      const data = toData(auth[1]);
-      const allImages = await Image.findAll();
-      res.json(allImages);
-    } catch (e) {
-      res.status(400).send("Invalid JWT token");
-    }
-  } else {
-    res.status(401).send({
-      message: "Please supply some valid credentials",
-    });
-  }
-});
 
 router.get("/", async (req, res, next) => {
   try {
@@ -45,13 +27,14 @@ router.get("/:imageId", async (req, res, next) => {
   }
 });
 
-router.post("/newimage", async (req, res, next) => {
+router.post("/:userId/newimage", async (req, res, next) => {
   try {
-    const { title, url } = req.body;
-    if (!title || !url) {
-      res.status(400).send("must provide correct paramaters");
+    const userId = parseInt(req.params.userId);
+    const user = await User.findByPk(userId);
+    if (!user) {
+      response.status(404).send("User not found");
     } else {
-      const newImage = await Image.create(req.body);
+      const newImage = await Image.create({ userId, ...req.body });
       res.json(newImage);
     }
   } catch (error) {
