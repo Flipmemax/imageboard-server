bd4a2d2904641eddc89ac3b7b6e23b27ec52104c
Flipmemax <maxphoogland@gmail.com>
2021-05-20 13:26:50 +0200

    add jsonParser to index.js

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 4000;
-
+const jsonParser = express.json();
 const userRouter = require("./routers/user");
 const imageRouter = require("./routers/image");
 
+//parses body to json format
+app.use(jsonParser);
+
+//routers:
 app.use("/users", userRouter);
 app.use("/images", imageRouter);
 
