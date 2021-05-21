893069f28d413a37dc5b6e7e5ebee21ab64b1b78
Flipmemax <maxphoogland@gmail.com>
2021-05-21 10:20:44 +0200

    changes on router endpoints

--- routers/image.js
+++ routers/image.js
@@ -1,8 +1,6 @@
-const { Router, response } = require("express");
+const { Router } = require("express");
 const Image = require("../models").image;
-const User = require("../models").user;
 const router = new Router();
-const authMiddleware = require("../auth/middleware");
 
 router.get("/", async (req, res, next) => {
   const limit = Math.min(req.query.limit || 25, 500);
@@ -23,39 +21,6 @@ router.get("/:imageId", async (req, res, next) => {
       res.status(404).send("No images found");
     } else {
       res.send(image);
-    }
-  } catch (error) {
-    next(error);
-  }
-});
-
-router.get("/:userId/imagefeed", async (req, res, next) => {
-  const limit = Math.min(req.query.limit || 25, 500);
-  const offset = req.query.offset || 0;
-  try {
-    const userId = parseInt(req.params.userId);
-    const user = await User.findByPk(userId, {
-      include: { model: Image, limit, offset },
-    });
-    if (user) {
-      res.send(user);
-    } else {
-      res.status(404).send("User not found");
-    }
-  } catch (error) {
-    next(error);
-  }
-});
-
-router.post("/:userId/newimage", authMiddleware, async (req, res, next) => {
-  try {
-    const userId = parseInt(req.params.userId);
-    const user = await User.findByPk(userId);
-    if (!user) {
-      response.status(404).send("User not found");
-    } else {
-      const newImage = await Image.create({ userId, ...req.body });
-      res.json(newImage);
     }
   } catch (error) {
     next(error);
--- routers/user.js
+++ routers/user.js
@@ -1,6 +1,8 @@
 const { Router } = require("express");
 const User = require("../models").user;
+const Image = require("../models").image;
 const bcrypt = require("bcrypt");
+const authMiddleware = require("../auth/middleware");
 
 const router = new Router();
 
@@ -10,6 +12,39 @@ router.get("/", async (req, res, next) => {
   try {
     const result = await User.findAndCountAll({ limit, offset });
     res.send({ users: result.rows, total: result.count });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/:userId/imagefeed", async (req, res, next) => {
+  const limit = Math.min(req.query.limit || 25, 500);
+  const offset = req.query.offset || 0;
+  try {
+    const userId = parseInt(req.params.userId);
+    const user = await User.findByPk(userId, {
+      include: { model: Image, limit, offset },
+    });
+    if (user) {
+      res.send(user);
+    } else {
+      res.status(404).send("User not found");
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/:userId/newimage", authMiddleware, async (req, res, next) => {
+  try {
+    const userId = parseInt(req.params.userId);
+    const user = await User.findByPk(userId);
+    if (!user) {
+      response.status(404).send("User not found");
+    } else {
+      const newImage = await Image.create({ userId, ...req.body });
+      res.json(newImage);
+    }
   } catch (error) {
     next(error);
   }
